78521992c2808ca4a46c04bd4a6453a1fdf2efc5
Naymul Hasan Fahim <imfahim05@gmail.com>
2020-08-08 10:16:52 +0600

    done

---
+++ array.js
@@ -0,0 +1,5 @@
+const nums = [1,4,5,56,56,5,6,7,7,7,7,8,7];
+const part = nums.slice(2,4);
+const remove = nums.splice(2,3,45,67,89,45,67,23);
+const together = nums.join(",");
+console.log(together);
---
+++ scope.js
@@ -0,0 +1,8 @@
+let bonus = 1222;
+function sum(num1, num2){
+    let result = num1 + num2 + bonus;
+    return result;
+   
+}
+const output = sum(3,5);
+console.log(output);
